3bff7b2eadaa62bf9ad2562a22cba16380869f09
Alejandro Trujillo <alejandrotrujillo9696@gmail.com>
2026-09-16 03:55:48 -0500

    Corregir ruta base para despliegue en Render

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/E-commerce_react/' : '/',
+  base:'/',
   server: {
     host: '0.0.0.0',
     port: 5173,
