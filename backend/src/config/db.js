3db31aaf0d69af2490b10a61a18e8de53bb9e83d
Alejandro Trujillo <alejandrotrujillo9696@gmail.com>
2026-09-16 02:36:57 -0500

    Preparar backend para conexion segura con Aiven

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,4 +1,5 @@
 import mysql from 'mysql2/promise';
+import fs from 'fs';
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -6,6 +7,12 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    ca: fs.readFileSync(process.env.DB_SSL_CA),
+    rejectUnauthorized: true,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
